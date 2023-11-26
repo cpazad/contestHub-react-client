69f808effa3e49ef0d4c594bce279bd4b8930788
cpazad <cpazad@gmail.com>
2023-11-26 19:06:46 +0600

    Private router created

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import { Navigate, useLocation } from "react-router-dom"
+import useAuth from "../Hooks/useAuth"
+import PropTypes from 'prop-types'
+
+
+const PrivateRoute = ({children}) => {
+    const {user, loading} =useAuth()
+    const location = useLocation()
+    if(loading){
+        return <span className="loading loading-spinner text-info"></span>
+    }
+    if(user){
+        return children
+    }
+    return (
+    <Navigate to="/login" state={{from:location}} replace></Navigate>
+  )
+}
+
+export default PrivateRoute
+
+PrivateRoute.propTypes ={
+    children:PropTypes.node
+}
